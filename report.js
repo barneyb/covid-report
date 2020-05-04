8c59295c767abf90f42dfb9847b127605ebefb3c
Barney Boisvert <bboisvert@gmail.com>
2020-05-04 12:37:44 -0700

    pull series as a orthogonal concept

--- report.js
+++ report.js
@@ -1,4 +1,6 @@
 function init(rawData) {
+    const HunThou = 100000;
+    const Week = 7;
     const formatDate = ld => {
         const ps = ld.split("-")
             .map(p => parseInt(p, 10));
@@ -8,107 +10,62 @@ function init(rawData) {
                 day: "numeric",
             });
     };
-    const NumFmt = new Intl.NumberFormat("en-US")
     const formatNumber = (v, places = 0) =>
-        NumFmt.format(v, {
+        new Intl.NumberFormat("en-US", {
             minimumFractionDigits: places,
             maximumFractionDigits: places,
-        });
-    const sumUp = supplier => rawData.jurisdictions
-        .map(supplier)
-        .reduce((s, n) => s + n, 0);
-    const tag = (el, c, attrs) =>
-        `<${el}${Object.keys(attrs || {})
-            .map(k => ` ${k === "className" ? "class" : k}="${attrs[k]}"`)
-            .join('')}>${c || ''}</${el}>`
-    const numTag = (el, v) =>
-        tag(el, formatNumber(v), {className: "number"});
-    const labelPointCells = () =>
-        tag('th', '', {colspan: 3})
-        + rawData.points
-            .map(p => {
-                let cs = 1;
-                if (p.deaths) cs += 1;
-                if (p.case_delta) {
-                    cs += 1;
-                    if (p.death_delta) cs += 1;
-                }
-                return tag(
-                    'th',
-                    formatDate(p.date),
-                    cs > 1 ? {colspan: cs} : null,
-                );
-            })
-            .join("");
-    const sublabelPointCells = () =>
-        tag('th')
-        + tag('th', 'Jurisdiction')
-        + tag('th', 'Population')
-        + rawData.points
-            .flatMap(p => {
-                const cs = [
-                    'Cases',
-                ];
-                if (p.case_delta) {
-                    cs.push('New')
-                }
-                if (p.deaths) {
-                    cs.push('Deaths');
-                    if (p.death_delta) {
-                        cs.push('New')
-                    }
-                }
-                return cs;
-            })
-            .map(c => tag('th', c))
-            .join("");
-    const renderDataRecord = (rec, el, num) =>
-        tag(el, num)
-        + tag(el, rec.name)
-        + numTag(el, rec.pop)
-        + rawData.points
-            .flatMap((p, i) => {
-                const d = rec.data[i]
-                const cs = [
-                    d.cases,
-                ];
-                if (p.case_delta) {
-                    cs.push(d.new_cases);
-                }
-                if (p.deaths) {
-                    cs.push(d.deaths)
-                    if (p.death_delta) {
-                        cs.push(d.new_deaths)
-                    }
-                }
-                return cs;
-            })
-            .map(c => numTag(el, c))
-            .join("");
-    const injectRows = (node, rows) =>
-        node.innerHTML = rows.map(it => `<tr>${it}</tr>`).join("\n");
-    const $ = document.querySelector.bind(document);
-    const head = $("#main-table thead");
-    const body = $("#main-table tbody");
-    const foot = $("#main-table tfoot");
-    $("#updated").innerText = "Updated " + formatDate(rawData.date);
-    const dataRecords = [{
-        name: "Total",
-        pop: sumUp(j => j.population),
-        data: rawData.points.map((p, i) => {
-            const fs = {
-                cases: sumUp(j => j.data[i].cases),
-            }
-            if (p.deaths) fs.deaths = sumUp(j => j.data[i].deaths);
-            if (p.case_delta) {
-                fs.new_cases = sumUp(j => j.data[i].since.cases);
-                if (p.death_delta) {
-                    fs.new_deaths = sumUp(j => j.data[i].since.deaths);
-                }
-            }
-            return fs;
-        }),
-    }, ...rawData.jurisdictions
+        }).format(v);
+    const formatPercent = (v, places = 1) =>
+        formatNumber(v * 100, places) + "%";
+
+    // expr is "(d, p, j) => value"
+    const series = [
+        {
+            name: "Cases",
+            expr: d => d.cases,
+        },
+        {
+            name: "Case Rate",
+            expr: (d, p, j) => d.cases / j.pop * HunThou,
+        },
+        {
+            name: "New Cases",
+            test: p => p.case_delta,
+            expr: d => d.new_cases,
+        },
+        {
+            name: "New Cases Rate",
+            test: p => p.case_delta,
+            expr: (d, p, j) => d.new_cases / p.days * Week / j.pop * HunThou,
+        },
+        {
+            name: "Deaths",
+            test: p => p.deaths,
+            expr: d => d.cases,
+        },
+        {
+            name: "Death Rate",
+            test: p => p.deaths,
+            expr: (d, p, j) => d.deaths / j.pop * HunThou,
+        },
+        {
+            name: "New Deaths",
+            test: p => p.death_delta,
+            expr: d => d.new_deaths,
+        },
+        {
+            name: "Case Mortality",
+            test: p => p.deaths,
+            expr: d => d.deaths / d.cases,
+            format: formatPercent,
+        },
+    ];
+    series.forEach(s => {
+        if (s.test == null) s.test = () => true;
+        if (s.format == null) s.format = formatNumber;
+    });
+
+    const dataRecords = rawData.jurisdictions
         .map(j => ({
             name: j.name,
             pop: j.population,
@@ -126,8 +83,74 @@ function init(rawData) {
                 }
                 return fs;
             }),
-        })),
-    ];
+        }));
+    const sumUp = supplier => dataRecords
+        .map(supplier)
+        .reduce((s, n) => s + n, 0);
+    dataRecords.unshift({
+        name: "Total",
+        pop: sumUp(j => j.pop),
+        data: rawData.points.map((p, i) => {
+            const fs = {
+                cases: sumUp(j => j.data[i].cases),
+            }
+            if (p.deaths) fs.deaths = sumUp(j => j.data[i].deaths);
+            if (p.case_delta) {
+                fs.new_cases = sumUp(j => j.data[i].new_cases);
+                if (p.death_delta) {
+                    fs.new_deaths = sumUp(j => j.data[i].new_deaths);
+                }
+            }
+            return fs;
+        }),
+    })
+
+    const tag = (el, c, attrs) =>
+        `<${el}${Object.keys(attrs || {})
+            .map(k => ` ${k === "className" ? "class" : k}="${attrs[k]}"`)
+            .join('')}>${c || ''}</${el}>`
+    const numTag = (el, v, fmt = formatNumber) =>
+        tag(el, fmt(v), {className: "number"});
+    const labelPointCells = () =>
+        tag('th', '', {colspan: 3})
+        + rawData.points
+            .map(p => {
+                const cs = series
+                    .filter(s => s.test(p))
+                    .length;
+                return tag(
+                    'th',
+                    formatDate(p.date),
+                    cs > 1 ? {colspan: cs} : null,
+                );
+            })
+            .join("");
+    const sublabelPointCells = () =>
+        tag('th')
+        + tag('th', 'Jurisdiction')
+        + tag('th', 'Population')
+        + rawData.points
+            .flatMap(p => series
+                .filter(s => s.test(p))
+                .map(s => tag('th', s.name)))
+            .join("");
+    const renderDataRecord = (rec, el, num) =>
+        tag(el, num)
+        + tag(el, rec.name)
+        + numTag(el, rec.pop)
+        + rawData.points
+            .flatMap((p, i) =>
+                series
+                    .filter(s => s.test(p))
+                    .map(s => numTag(el, s.expr(rec.data[i], p, rec), s.format)))
+            .join("");
+    const injectRows = (node, rows) =>
+        node.innerHTML = rows.map(it => `<tr>${it}</tr>`).join("\n");
+    const $ = document.querySelector.bind(document);
+    const head = $("#main-table thead");
+    const body = $("#main-table tbody");
+    const foot = $("#main-table tfoot");
+    $("#updated").innerText = "Updated " + formatDate(rawData.date);
     const render = () => {
         injectRows(head, [
             labelPointCells(),
