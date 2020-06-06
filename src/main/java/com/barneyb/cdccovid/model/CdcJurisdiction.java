0a2a2c52f108c280245967f6e07ec020b1533fb1
Barney Boisvert <bboisvert@gmail.com>
2020-06-06 10:55:25 -0700

    CDC's differentiating confirmed/probably cases and deaths

--- src/main/java/com/barneyb/cdccovid/model/CdcJurisdiction.java
+++ src/main/java/com/barneyb/cdccovid/model/CdcJurisdiction.java
@@ -13,8 +13,16 @@ public class CdcJurisdiction {
     private String range;
     @JsonProperty("Cases Reported")
     private Object cases;
+    @JsonProperty("Confirmed Cases Reported")
+    private Object confirmedCases;
+    @JsonProperty("Probable Cases Reported")
+    private Object probableCases;
     @JsonProperty("Deaths")
     private Object deaths;
+    @JsonProperty("Confirmed Deaths")
+    private Object confirmedDeaths;
+    @JsonProperty("Probable Deaths")
+    private Object probableDeaths;
     @JsonProperty("Community Transmission")
     private String community;
     @JsonProperty("URL")
