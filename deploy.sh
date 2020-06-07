8af2f6dcd6e535384277fae25d41c699bb815054
Barney Boisvert <bboisvert@gmail.com>
2020-06-06 22:08:39 -0700

    support client-only deploys

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,26 @@
 
 cd `dirname $0`
 
-mvn clean package
-cp target/*.jar covid.jar
+DO_JAR=0
+
+while [ "$1" != "" ]; do
+  case $1 in
+    "--client-only")
+      shift
+      DO_JAR=1
+      ;;
+    *)
+      echo "Usage `basename $0` [ --client-only ]"
+      exit 1
+  esac
+done
+
+if [ $DO_JAR -eq 0 ]; then
+  mvn clean package
+  cp target/*.jar covid.jar
+fi
+
+
 rsync -a \
   --progress --stats \
   *.html \
@@ -14,4 +32,3 @@ rsync -a \
   covid.jar \
   hopkins \
   barneyb.com:/vol/www/static/covid/
-rm covid.jar
