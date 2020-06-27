43a7684ef527f922780df2efd5b1f296092a3e5e
Barney Boisvert <bboisvert@gmail.com>
2020-06-27 10:57:51 -0700

    redo the dashboard as a grid of cards

--- index.js
+++ index.js
@@ -15,81 +15,103 @@ function init(data) {
             zero[0] - (zero[0] - end[0]) * (val > 0 ? Math.sqrt(dist) : dist),
             zero[1] - (zero[1] - end[1]) * (val > 0 ? Math.sqrt(dist) : dist),
             zero[2] - (zero[2] - end[2]) * dist,
-        ];
+        ].map(d => Math.round(d * 100) / 100);
     }
+    const formatHsl = (h, s, l) =>
+        `hsl(${formatNumber(h, 2)},${formatNumber(s, 2)}%,${formatNumber(l, 2)}%)`;
     const drawColumn = segments => {
         const width = 30;
-        const height = 132;
-        const pad = 1;
+        const height = 150;
         const ds = segments
             .map(it => ({name: it[0], pop: it[1], delta: it[2]}))
             .filter(it => isActualNumber(it.delta))
             .sort((a, b) => b.delta - a.delta);
         const tp = ds.reduce((s, d) => s + d.pop, 0);
-        const factor = (height - pad - pad) / tp;
-        return el('svg', {width:width + "px",height:height + "px"}, ds.reduce((agg, it) => {
-            const [h,s,l] = colorForDelta(it.delta);
-            return {
-                pop: agg.pop + it.pop,
-                rects: agg.rects.concat(el('rect', {
-                    x: pad,
-                    y: pad + Math.round(agg.pop * factor * 100) / 100,
-                    width: width - pad - pad,
-                    height: Math.round(it.pop * factor * 100) / 100,
-                    fill: `hsl(${h},${s}%,${l}%)`,
-                    onmouseover: `this.style.fill='hsl(270,${s}%,${l}%)'`,
-                    onmouseout: `this.style.fill='hsl(${h},${s}%,${l}%)'`,
-                }, [
-                    el('title', it.name + " (" + formatPercent(it.delta, 1, true) + ", week-over-week)"),
-                ])),
-            };
-        }, {pop: 0, rects: []}).rects)
+        const factor = height / tp;
+        return el('svg',
+            {
+                className: "segments",
+                viewBox: `0 0 ${width} ${height}`,
+                preserveAspectRatio: "none",
+            },
+            ds.reduce((agg, it) => {
+                const [h,s,l] = colorForDelta(it.delta);
+                return {
+                    pop: agg.pop + it.pop,
+                    rects: agg.rects.concat(el('rect', {
+                        x: 0,
+                        y: agg.pop * factor,
+                        width: width,
+                        height: it.pop * factor,
+                        fill: formatHsl(h, s, l),
+                        onmouseover: `this.style.fill='${formatHsl(270, s, l)}'`,
+                        onmouseout: `this.style.fill='${formatHsl(h, s, l)}'`,
+                    }, [
+                        el('title', it.name + " (" + formatPercent(it.delta, 1, true) + ", week-over-week)"),
+                    ])),
+                };
+            }, {pop: 0, rects: []}).rects)
     }
     const drawSpark = values => {
         const width = 200;
         const height = 50;
-        const pad = 1;
+        const stroke = 3;
         const min = values.reduce((a, b) => Math.min(a, b), 999999999)
         const max = values.reduce((a, b) => Math.max(a, b), 0)
         const range = max - min;
         const len = values.length
-        const dx = (width - pad - pad) / (len - 1)
+        const dx = width / (len - 1)
         const points = values
             .map((d, i) => [
-                pad + i * dx,
-                height - pad - (d - min) / range * (height - pad - pad)
+                i * dx,
+                formatNumber(height - (d - min) / range * height, 2)
             ])
             .map(p => p.join(","))
             .join(" ");
         const first = values[0]
         const last = values[len - 1]
         const [h,s,l] = colorForDelta((last - first) / first);
-        return tag('svg', [
-                tag('title', `Average new cases per day (past ${len} days)`),
-                tag('polyline', '', {points,fill:"none",stroke:`hsl(${h},${s+10}%,${l-10}%)`,'stroke-width':"2px"}),
-            ], {width:width + "px",height:height + "px"});
+        return el(
+            'svg',
+            {
+                viewBox: `${-stroke} ${-stroke} ${width + 2 * stroke} ${height + 2 * stroke}`,
+            },
+            [
+                el('title', `Average new cases per day (past ${len} days)`),
+                el('polyline', {
+                    points,
+                    fill: "none",
+                    stroke: formatHsl(h, s + 10, l - 10),
+                    'stroke-width': stroke + "px",
+                    'stroke-linejoin': "round",
+                    'stroke-linecap': "round",
+                }),
+            ]);
     };
-    const drawNum = (label, n) => {
-        return tag('div', [
-            tag('span', label + ":", {style: "font-size:90%;float:left;margin-top:0.5em"}),
-            tag('span', formatNumber(n), {style: "font-weight:bold;font-size:200%"}),
-        ], {style: "text-align:right;margin-top:0.25em"});
+    const drawNum = (label, n, title) => {
+        return el('div', {className: "stat", title}, [
+            el('div', {className: "stat-label"}, label + ":"),
+            el('div', {className: "stat-value"}, formatNumber(n)),
+        ]);
     }
     const renderAreas = (element, areas) => {
         element.innerHTML = areas
             .map(a => {
                 const cols = [];
-                cols.push(tag('div', [
-                    drawSpark(a.values),
-                    drawNum("Daily", a.daily),
-                    drawNum("Total", a.total),
-                    ], {className: "spark"}));
+                cols.push(el('div', {className: "spark"}, [
+                    el('header',[
+                        el('h3', {title:a.name}, a.name),
+                    ]),
+                    el('div', {className: "spark-container"}, drawSpark(a.values)),
+                    drawNum("Cases", a.total, "Total cases"),
+                    drawNum("Daily", a.daily, "New cases per day, on average, for the past seven days"),
+                    a.pop && drawNum("Rate", a.total / a.pop * HunThou, "Cases per 100,000 population"),
+                    ]));
                 if (a.segments) {
-                    cols.push(drawColumn(a.segments));
+                    cols.push(el('div', {className: "segment-container"}, drawColumn(a.segments)));
                 }
-                return tag('h3', a.name) +
-                    tag('div', cols
-                        .map(c => tag('div', c, {className: "col"})), {className: "colset"})
+                return el('div', {className: "card"}, cols
+                        .map(c => el('div', {className: "col"}, c)))
             })
             .join("\n")
     };
