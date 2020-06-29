5668fa33455f0874c161a3bb0cae554ff20a7222
Barney Boisvert <bboisvert@gmail.com>
2020-06-29 16:39:03 -0700

    inject Main's deps, instead of it fetching by type

--- src/main/java/com/barneyb/covid/Main.java
+++ src/main/java/com/barneyb/covid/Main.java
@@ -1,13 +1,11 @@
 package com.barneyb.covid;
 
 import com.barneyb.covid.hopkins.HopkinsTransform;
-import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Files;
@@ -18,9 +16,6 @@ public class Main implements ApplicationRunner {
 
     @Value("${covid-report.output.dir}")
     Path outputDir;
-
-    @Autowired
-    ApplicationContext appCtx;
 
     @Autowired
     @Qualifier("worldwide")
@@ -34,29 +29,36 @@ public class Main implements ApplicationRunner {
     @Qualifier("or")
     Store orStore;
 
+    @Autowired
+    Mortality mortality;
+
+    @Autowired
+    HopkinsTransform hopkinsTransform;
+
+    @Autowired
+    ReportJsonEmitter tableJson;
+
+    @Autowired
+    TsvEmitter tableTsv;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         if (args.containsOption("mortality")) {
-            appCtx.getBean(Mortality.class)
-                    .emit(Files.newBufferedWriter(Path.of("mortality.csv")));
+            mortality.emit(Files.newBufferedWriter(Path.of("mortality.csv")));
         }
 
         if (args.containsOption("hopkins")) {
-            appCtx.getBean(HopkinsTransform.class)
-                    .transform();
+            hopkinsTransform.transform();
         }
 
-        val json = appCtx.getBean(ReportJsonEmitter.class);
-        val tsv = appCtx.getBean(TsvEmitter.class);
+        tableJson.emit(Files.newOutputStream(outputDir.resolve("table-ww.json")), wwStore);
+        tableTsv.emit(Files.newOutputStream(outputDir.resolve("table-ww.tsv")), wwStore);
 
-        json.emit(Files.newOutputStream(outputDir.resolve("table-ww.json")), wwStore);
-        tsv.emit(Files.newOutputStream(outputDir.resolve("table-ww.tsv")), wwStore);
+        tableJson.emit(Files.newOutputStream(outputDir.resolve("table-us.json")), usStore);
+        tableTsv.emit(Files.newOutputStream(outputDir.resolve("table-us.tsv")), usStore);
 
-        json.emit(Files.newOutputStream(outputDir.resolve("table-us.json")), usStore);
-        tsv.emit(Files.newOutputStream(outputDir.resolve("table-us.tsv")), usStore);
-
-        json.emit(Files.newOutputStream(outputDir.resolve("table-or.json")), orStore);
-        tsv.emit(Files.newOutputStream(outputDir.resolve("table-or.tsv")), orStore);
+        tableJson.emit(Files.newOutputStream(outputDir.resolve("table-or.json")), orStore);
+        tableTsv.emit(Files.newOutputStream(outputDir.resolve("table-or.tsv")), orStore);
     }
 
 }
--- src/main/java/com/barneyb/covid/Mortality.java
+++ src/main/java/com/barneyb/covid/Mortality.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -53,6 +54,7 @@ public class Mortality {
     JdbcTemplate jdbcTmpl;
 
     @Autowired
+    @Qualifier("us")
     Store store;
 
     public void update() {
