e76bd2f8d3b7f047e39744b6088c923b7f1e81aa
Barney Boisvert <bboisvert@gmail.com>
2020-07-02 07:08:37 -0700

    include errata and override changes in the refresh output

--- hopkins/refresh.sh
+++ hopkins/refresh.sh
@@ -5,7 +5,22 @@ cd `dirname $0`
 
 cd ../../
 cd COVID-19
-git pull > /dev/null
+git fetch
+git checkout master
+echo "- Errata -------------------------------------------------------------"
+git diff --ignore-space-change --ignore-space-at-eol \
+  master origin/master -- \
+  csse_covid_19_data/csse_covid_19_time_series/Errata.csv
+echo "----------------------------------------------------------------------"
+git merge origin/master
+git checkout web-data
+echo "- Overrides ----------------------------------------------------------"
+git diff --ignore-space-change --ignore-space-at-eol \
+  web-data origin/web-data -- \
+  override/override.csv
+echo "----------------------------------------------------------------------"
+git merge origin/web-data
+git checkout master
 cd $OLDPWD
 docker run --rm -v `pwd`:/data -u `id -u` covid
 
