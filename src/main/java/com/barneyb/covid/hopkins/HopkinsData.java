a58e3c11c45900f2e127e151aeec5503755cce07
Barney Boisvert <bboisvert@gmail.com>
2020-07-02 19:57:30 -0700

    deal with Kansas City, and return Collections instead of Lists

--- src/main/java/com/barneyb/covid/hopkins/HopkinsData.java
+++ src/main/java/com/barneyb/covid/hopkins/HopkinsData.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Repository;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -37,95 +37,147 @@ public class HopkinsData {
         usDeathsFile = tsDir.resolve("time_series_covid19_deaths_US.csv");
     }
 
-    public List<UidLookup> loadUidLookup() {
-        return load(uidLookupFile, UidLookup.class);
+    public Collection<UidLookup> loadUidLookup() {
+        return loadUids(UidLookup.class);
     }
 
     /**
      * I exist as compatibility alias for {@link #loadUidLookup()}. Use that?
      */
-    public List<Demographics> loadDemographics() {
-        return load(uidLookupFile, Demographics.class);
+    public Collection<Demographics> loadDemographics() {
+        return loadUids(Demographics.class);
     }
 
-    public List<USTimeSeries> loadUSCases() {
+    private <T extends UidLookup> Collection<T> loadUids(Class<T> clazz) {
+        /*
+         * Munge Kansas City's counties' populations to avoid double-counting
+         * the city's residents, as the city is a only a reporting area for
+         * covid, not population. Assume an even distribution of the city across
+         * the counties, which is not correct, but it's at least arguable in the
+         * face of no better data.
+         */
+        final long popKansasCity = 488943;
+        final long popCass = 105780;
+        final long popClay = 249948;
+        final long popJackson = 703011;
+        final long popPlatte = 104418;
+        final long popTotal = popCass + popClay + popJackson + popPlatte;
+        final long adjustCass = (long) ((1.0 * popCass / popTotal) * popKansasCity);
+        final long adjustClay = (long) ((1.0 * popClay / popTotal) * popKansasCity);
+        final long adjustJackson = (long) ((1.0 * popJackson / popTotal) * popKansasCity);
+        final long adjustPlatte = (long) ((1.0 * popPlatte / popTotal) * popKansasCity);
+        return stream(uidLookupFile, clazz)
+                .map(d -> {
+                    if (!d.isLocality()) return d;
+                    if (!"Missouri".equals(d.getState())) return d;
+                    long adjust = 0;
+                    switch (d.getLocality()) {
+                        case "Cass":
+                            adjust = adjustCass;
+                            break;
+                        case "Clay":
+                            adjust = adjustClay;
+                            break;
+                        case "Jackson":
+                            adjust = adjustJackson;
+                            break;
+                        case "Platte":
+                            adjust = adjustPlatte;
+                            break;
+                    }
+                    if (adjust != 0) d.setPopulation(d.getPopulation() - adjust);
+                    return d;
+                })
+                .collect(Collectors.toList());
+    }
+
+    public Collection<USTimeSeries> loadUSCases() {
         return loadUS(usCasesFile);
     }
 
-    public List<USTimeSeries> loadUSDeaths() {
+    public Collection<USTimeSeries> loadUSDeaths() {
         return loadUS(usDeathsFile);
     }
 
-    private List<USTimeSeries> loadUS(Path src) {
+    private Collection<USTimeSeries> loadUS(Path src) {
         return stream(src, USTimeSeries.class)
                 // Exception type 1, such as recovered and Kansas City, ranging from 8407001 to 8407999.
                 // Dukes and Nantucket, Mass.
                 .filter(d -> {
                     if (!"Massachusetts".equals(d.getState())) return true;
-                    val l = d.getLocality();
-                    if ("Dukes".equals(l)) return false;
-                    if ("Nantucket".equals(l)) return false;
-                    return true;
+                    switch (d.getLocality()) {
+                        case "Dukes":
+                        case "Nantucket":
+                            return false;
+                        default:
+                            return true;
+                    }
                 })
-                // todo: Kansas City - KC has it's own counts, but its population is attributed to the counties it covers. :|
-                // todo: Michigan's state/federal corrections? Gonna trust they got rolled into the county the prisons are in. :|
+                // Kansas City - adjust the county populations KC is partially in
+                // Michigan's state/federal corrections are outside the counties they reside in; they are a legit locality, though without population data.
                 // Utah's districts
                 .filter(d -> {
                     if (!"Utah".equals(d.getState())) return true;
-                    val l = d.getLocality();
-                    // Bear River
-                    if ("Box Elder".equals(l)) return false;
-                    if ("Cache".equals(l)) return false;
-                    if ("Rich".equals(l)) return false;
-                    // Central Utah
-                    if ("Juab".equals(l)) return false;
-                    if ("Millard".equals(l)) return false;
-                    if ("Piute".equals(l)) return false;
-                    if ("Sanpete".equals(l)) return false;
-                    if ("Sevier".equals(l)) return false;
-                    if ("Wayne".equals(l)) return false;
-                    // Southeast Utah
-                    if ("Carbon".equals(l)) return false;
-                    if ("Emery".equals(l)) return false;
-                    if ("Grand".equals(l)) return false;
-                    // Southwest Utah
-                    if ("Beaver".equals(l)) return false;
-                    if ("Garfield".equals(l)) return false;
-                    if ("Iron".equals(l)) return false;
-                    if ("Kane".equals(l)) return false;
-                    if ("Washington".equals(l)) return false;
-                    // Tricounty
-                    if ("Daggett".equals(l)) return false;
-                    if ("Duchesne".equals(l)) return false;
-                    if ("Uintah".equals(l)) return false;
-                    // Weber-Morgan
-                    if ("Morgan".equals(l)) return false;
-                    if ("Weber".equals(l)) return false;
-                    return true;
+                    switch (d.getLocality()) {
+                        // Bear River
+                        case "Box Elder":
+                        case "Cache":
+                        case "Rich":
+                        // Central Utah
+                        case "Juab":
+                        case "Millard":
+                        case "Piute":
+                        case "Sanpete":
+                        case "Sevier":
+                        case "Wayne":
+                        // Southeast Utah
+                        case "Carbon":
+                        case "Emery":
+                        case "Grand":
+                        // Southwest Utah
+                        case "Beaver":
+                        case "Garfield":
+                        case "Iron":
+                        case "Kane":
+                        case "Washington":
+                        // Tricounty
+                        case "Daggett":
+                        case "Duchesne":
+                        case "Uintah":
+                        // Weber-Morgan
+                        case "Morgan":
+                        case "Weber":
+                            return false;
+                        default:
+                            return true;
+                    }
                 })
                 // Exception type 2, only the New York City, which is replacing New York County and its FIPS code.
                 .filter(d -> {
                     if (!"New York".equals(d.getState())) return true;
-                    val l = d.getLocality();
-                    if ("Bronx".equals(l)) return false;
-                    if ("Kings".equals(l)) return false;
-                    if ("Queens".equals(l)) return false;
-                    if ("Richmond".equals(l)) return false;
-                    return true;
+                    switch (d.getLocality()) {
+                        case "Bronx":
+                        case "Kings":
+                        case "Queens":
+                        case "Richmond":
+                            return false;
+                        default:
+                            return true;
+                    }
                 })
                 // todo: Exception type 3, Diamond Princess, US: 84088888; Grand Princess, US: 84099999.
                 .collect(Collectors.toList());
     }
 
-    public List<GlobalTimeSeries> loadGlobalCases() {
+    public Collection<GlobalTimeSeries> loadGlobalCases() {
         return load(globalCasesFile, GlobalTimeSeries.class);
     }
 
-    public List<GlobalTimeSeries> loadGlobalDeaths() {
+    public Collection<GlobalTimeSeries> loadGlobalDeaths() {
         return load(globalDeathsFile, GlobalTimeSeries.class);
     }
 
-    private <T> List<T> load(Path src, Class<T> clazz) {
+    private <T> Collection<T> load(Path src, Class<T> clazz) {
         return build(src, clazz).parse();
     }
 
