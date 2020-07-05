d17032ae358242f4ffa30a372fb46d2062fa2af9
Barney Boisvert <bboisvert@gmail.com>
2020-07-04 20:59:00 -0700

    send the git noise to /dev/null

--- hopkins/refresh.sh
+++ hopkins/refresh.sh
@@ -5,21 +5,21 @@ cd `dirname $0`
 
 cd ../../
 cd COVID-19
-git fetch --prune
+git fetch --prune > /dev/null
 git checkout master
 echo "- Errata -------------------------------------------------------------"
 git diff --ignore-space-change --ignore-space-at-eol \
   master origin/master -- \
   csse_covid_19_data/csse_covid_19_time_series/Errata.csv
 echo "----------------------------------------------------------------------"
-git merge origin/master
+git merge origin/master > /dev/null
 git checkout web-data
 echo "- Overrides ----------------------------------------------------------"
 git diff --ignore-space-change --ignore-space-at-eol \
   web-data origin/web-data -- \
   override/override.csv
 echo "----------------------------------------------------------------------"
-git merge origin/web-data
+git merge origin/web-data > /dev/null
 git checkout master
 cd $OLDPWD
 docker run --rm -v `pwd`:/data -u `id -u` covid
