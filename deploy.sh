3cc391d340574a0cb623a043b3d03e0deb63d8a0
Barney Boisvert <bboisvert@gmail.com>
2020-08-02 20:11:56 -0700

    do a kludgey content hash thing on JS/CSS assets

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 cd `dirname $0`
 
+SRC_DIR=`pwd`/src/main/webapp
 LOCAL_DIR=`pwd`/target/client
 REMOTE_HOST=barneyb.com
 REMOTE_DIR=/vol/www/static/covid/
@@ -25,26 +26,49 @@ while [ "$1" != "" ]; do
   esac
 done
 
-
 if [ $DO_JAR -eq 0 ]; then
   mvn clean package
   mkdir -p $LOCAL_DIR
-  cp target/*.jar $LOCAL_DIR/covid.jar
 else
   rm -rf $LOCAL_DIR
   mkdir -p $LOCAL_DIR
 fi
+cp target/*.jar $LOCAL_DIR/covid.jar
+mkdir $LOCAL_DIR/hopkins
+cp $SRC_DIR/hopkins/refresh.sh $LOCAL_DIR/hopkins
 
-rsync -a \
-  --exclude data \
-  src/main/webapp/* \
-  $LOCAL_DIR
+declare -A assets
+# find all JS/CSS assets
+for a in `find $SRC_DIR -name "*.html" \
+  | xargs cat \
+  | egrep '<(script src=|link href=)"' \
+  | sed -e 's/.*\(script src\|link href\)="\([^"]*\)".*$/\2/' \
+  | sort -u`; do
+    assets[$a]=`shasum $SRC_DIR/$a | cut -c 1-10`
+done
+# compute hashes for each file
+for a in "${!assets[@]}"; do
+  IFS='.' read -r -aparts <<< "$a"
+  fn=${parts[0]}.${assets[$a]}.${parts[1]}
+  assets[$a]=$fn
+done
+# move all the HTMLs over as-is
+for a in `find $SRC_DIR -name "*.html"`; do
+  cp $a $LOCAL_DIR
+done
+# copy each asset over, replace it's refs in the HTMLs
+for a in "${!assets[@]}"; do
+  cp $SRC_DIR/$a $LOCAL_DIR/${assets[$a]}
+  sed -i -e "s/$a/${assets[$a]}/" $LOCAL_DIR/*.html
+done
 
 pushd $LOCAL_DIR
-rsync -a \
+rsync --archive \
+  --delete \
+  --exclude data \
+  --exclude stage \
   --progress --stats \
-  --delete-after \
-  * \
+  ./ \
   $REMOTE_HOST:$REMOTE_DIR
 popd
 
