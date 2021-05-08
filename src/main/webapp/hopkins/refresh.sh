8f678478d2ee1417b4f421bce446695fb9443400
Barney Boisvert <bboisvert@gmail.com>
2021-05-08 08:57:45 -0700

    don't print errata when refreshing

--- src/main/webapp/hopkins/refresh.sh
+++ src/main/webapp/hopkins/refresh.sh
@@ -7,11 +7,11 @@ cd ../../
 cd COVID-19
 git fetch --prune >/dev/null
 git checkout master
-echo "- Errata -------------------------------------------------------------"
-git diff --ignore-space-change --ignore-space-at-eol \
-    master origin/master -- \
-    csse_covid_19_data/csse_covid_19_time_series/Errata.csv
-echo "----------------------------------------------------------------------"
+#echo "- Errata -------------------------------------------------------------"
+#git diff --ignore-space-change --ignore-space-at-eol \
+#    master origin/master -- \
+#    csse_covid_19_data/csse_covid_19_time_series/Errata.csv
+#echo "----------------------------------------------------------------------"
 git merge origin/master >/dev/null
 cd $OLDPWD
 docker run --rm -v "$(pwd)":/data -u "$(id -u)" -w /data/covid openjdk:11 \
